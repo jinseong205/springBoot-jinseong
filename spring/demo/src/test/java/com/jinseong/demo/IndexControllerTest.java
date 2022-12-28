e6e5d201a6e9e92a2a75bf708bf50bcc9b0da30d
jinseong <57009926+jinseong205@users.noreply.github.com>
2022-12-28 21:24:38 +0900

    ^^7

--- spring/demo/src/test/java/com/jinseong/demo/IndexControllerTest.java
+++ spring/demo/src/test/java/com/jinseong/demo/IndexControllerTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -27,6 +31,15 @@ public class IndexControllerTest {
         resultActions.andExpect(status().isOk()); // alt+shift+o
         assertThat(status().isOk());
         assertThat("기대하는값").isEqualTo("기대하는값");
+    }
 
+    @Test
+    public void test() throws Exception{
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        List<Integer> newList = list.stream().map((e)-> e+1).collect(Collectors.toList());
+        
+        newList.stream().forEach((e)->{
+            System.out.print(e + "\t");
+        });
     }
 }
